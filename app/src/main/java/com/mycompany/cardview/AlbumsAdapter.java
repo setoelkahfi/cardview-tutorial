4bedcd8409670cd9733362c785d6c9851441697c
Seto Elkahfi <setoelkahfi@gmail.com>
2016-12-02 14:33:42 +0700

    Initial commit

---
+++ app/src/main/java/com/mycompany/cardview/AlbumsAdapter.java
@@ -0,0 +1,96 @@
+package com.mycompany.cardview;
+
+import android.content.Context;
+import android.support.v7.widget.PopupMenu;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+import java.util.Locale;
+
+
+public class AlbumsAdapter extends RecyclerView.Adapter<AlbumsAdapter.MyViewHolder> {
+
+    private Context mContext;
+    private List<Album> mAlbumList;
+
+    public AlbumsAdapter(Context mContext, List<Album> mAlbumList) {
+        this.mContext = mContext;
+        this.mAlbumList = mAlbumList;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.album_card, parent, false);
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(final MyViewHolder holder, int position) {
+        Album album = mAlbumList.get(position);
+        holder.title.setText(album.getName());
+        holder.count.setText(String.format(Locale.getDefault(), "%d songs", album.getNumOfSongs()));
+
+        Glide.with(mContext).load(album.getThumbnail()).into(holder.thumbnail);
+
+        holder.overflow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showPopupMenu(holder.overflow);
+            }
+        });
+    }
+
+    private void showPopupMenu(View view) {
+
+        // Inflate menu
+        PopupMenu popup = new PopupMenu(mContext, view);
+        MenuInflater inflater = popup.getMenuInflater();
+        inflater.inflate(R.menu.menu_album, popup.getMenu());
+        popup.setOnMenuItemClickListener(new MyMenuItemClickListener());
+        popup.show();
+    }
+
+    private class MyMenuItemClickListener implements PopupMenu.OnMenuItemClickListener {
+
+        @Override
+        public boolean onMenuItemClick(MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.action_add_favorite:
+                    Toast.makeText(mContext, "Add to favorite", Toast.LENGTH_SHORT).show();
+                    return true;
+                case R.id.action_play_next:
+                    Toast.makeText(mContext, "Play Next", Toast.LENGTH_SHORT).show();
+                    return true;
+            }
+            return false;
+        }
+    }
+    @Override
+    public int getItemCount() {
+        return mAlbumList.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView title, count;
+        public ImageView thumbnail, overflow;
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+            title = (TextView) itemView.findViewById(R.id.title);
+            count = (TextView) itemView.findViewById(R.id.count);
+            thumbnail = (ImageView) itemView.findViewById(R.id.thumbnail);
+            overflow = (ImageView) itemView.findViewById(R.id.overflow);
+        }
+    }
+}
